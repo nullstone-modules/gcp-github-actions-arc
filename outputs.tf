69bbb3789696b3d224e8724e5e8a9402d3d112ea
Brad Sickles <brad.sickles@gmail.com>
2025-12-15 15:07:32 -0500

    Fixed github_config_secret output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,5 @@
 output "github_config_secret" {
-  value = {
-    name      = kubernetes_secret_v1.github_app.metadata[0].name
-  }
-
+  value       = kubernetes_secret_v1.github_app.metadata[0].name
   description = "string ||| The name of a Kubernetes secret containing the config to connect to a GitHub App. This secret is in the default runners namespace."
 }
 
